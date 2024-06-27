670b1df613fea3c67db1a66669021a43d34628c9
SimonaAda <166747901+SimonaAda@users.noreply.github.com>
2024-06-27 16:45:59 +0200

    op

--- Xml.cs
+++ Xml.cs
@@ -18,11 +18,20 @@ namespace pF
 
     public static List<Skincare> ImportFromXml(string filePath)
     {
-        XmlSerializer serializer = new XmlSerializer(typeof(List<Skincare>));
-        using (TextReader reader = new StreamReader(filePath))
-        {
-            return (List<Skincare>)serializer.Deserialize(reader);
-        }
+            List<Skincare> skincares = [];
+            if (File.Exists(filePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Skincare>));
+                using (TextReader reader = new StreamReader(filePath))
+                {
+                    skincares = (List<Skincare>)serializer.Deserialize(reader);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Nemohl jsem najít soubor na importovaní.Oveřte, že je na správném místě {filePath}");
+            }
+            return skincares;
     }
 
     }
